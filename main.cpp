d2119d8cf9c67dab5196d38d3cb79edc0dae190f
AlexBrence <alexbrence@protonmail.com>
2021-10-23 14:23:31 +0200

    Game init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Game.h"
+
+int main() {
+
+    // Init game
+    Game game;
+
+    // Game loop
+    while (game.running()) {
+        game.update();
+
+        game.render();
+    }
+
+    return 0;
+}
